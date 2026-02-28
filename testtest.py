7e600466555c41c519cf07d4cbbde86c7531596e
Josef Kuchař <josef.kuchar267@gmail.com>
2026-02-28 16:50:38 +0100

    chore: cleanup

--- testtest.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-
-dataset = load_dataset("mikronai/svg-svgrepo")
-print(dataset)
-print("train", len(dataset["train"]))
-print("validation", len(dataset["valid"]))
-print("test", len(dataset["test"]))
-print(dataset["train"][0])
